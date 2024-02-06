e7b364a75be73582ff1094d5f1039de03f7b44b4
EstebanCardenas27 <estebancardenas2796@gmail.com>
2024-02-06 14:31:02 -0300

    #1 V1.0.1: "Componente_Seleccion_Metodo_De_Pago"

---
+++ src/Twig/Components/BtnPaymentMethod.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Twig\Components;
+
+use Symfony\UX\LiveComponent\Attribute\AsLiveComponent;
+use Symfony\UX\LiveComponent\DefaultActionTrait;
+
+
+
+#[AsLiveComponent]
+final class BtnPaymentMethod
+{
+    public string $paymentMethod;
+    use DefaultActionTrait;
+}
